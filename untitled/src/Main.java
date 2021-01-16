6108ffbcf8fb264a3c21dd92d9c6a654c93b0495
troffimovka <troffimovka@gmail.com>
2021-01-16 19:32:12 +0300

    11

---
+++ untitled/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentAcc = 2_000_000_000;
+        int transh = 500_000_000;
+        int Total = currentAcc + transh;
+        System.out.println(Total);
+    }
+}
